f68e03ef5f503fa1ce139185f53bec55a2b11a5c
Diego Malta <diego_malta@hotmail.com>
2023-04-23 10:59:13 -0500

    moving web to its folder

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import RsvpForm from './components/RsvpForm/RsvpForm';
+
+function App() {
+  return (
+    <div className='w-full max-w-xs'>
+      <RsvpForm />
+      <p className='text-center text-gray-500 text-xs'>All Right reserved.</p>
+    </div>
+  );
+}
+
+export default App;
