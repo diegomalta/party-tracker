d516437f9e85daceecb3744e69684e2a5046ecf3
Diego Malta <diego_malta@hotmail.com>
2023-05-01 23:08:14 -0500

    welcome message WIP!

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import RsvpForm from './components/RsvpForm/RsvpForm';
+import RsvpForm from './views/RsvpForm/RsvpForm';
 
 function App() {
   return (
-    <div className='w-full max-w-xs'>
+    <div className='container mx-auto'>
       <RsvpForm />
-      <p className='text-center text-gray-500 text-xs'>All Right reserved.</p>
+      <p className='text-center text-gray-500 text-xs'>Lina's Party all Right reserved.</p>
     </div>
   );
 }
---
+++ web/src/components/WelcomeMessage/WelcomeMessage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const WelcomeMessage = (props: any) => {
+    return (<div className="text-center py-8">
+        <h1 className="text-4xl font-bold tracking-tight text-gray-900">Someone, You're invited to x's party! &#127881;</h1>
+    </div>)
+}
+
+export default WelcomeMessage;
